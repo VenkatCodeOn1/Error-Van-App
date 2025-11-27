934ff7d4e8993d2829f79804039037d30d111a29
VenkatCodeOn1 <imvenkat01@gmail.com>
2025-11-27 16:08:44 +0530

    Add files via upload
    First Commit

---
+++ src/main/java/org/errorvan/LocationInfoHelper.java
@@ -0,0 +1,12 @@
+
+package org.errorvan;
+
+public class LocationInfoHelper {
+        String friendlyIdentifier;
+        String collectionType;
+
+        LocationInfoHelper(String friendlyIdentifier, String collectionType) {
+            this.friendlyIdentifier = friendlyIdentifier;
+            this.collectionType = collectionType;
+        }
+}
